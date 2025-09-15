734f07430160b76c178857ffc3b0a14dc8f4197e
kyuubi-Yhp <south01village@gmail.com>
2025-09-15 23:46:45 +0300

    добавлен обработчик радикнопки, так же начинается работа с массивом интересов в состоянии обьекта

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,10 @@ function App() {
     surname: 'rukal',
     age: 29,
     sex: 'male',
-    hobbies: ['ytube', 'front', 'curriera'],
+    interest: ['ytube', 'front', 'curriera'],
   })
 
-  const [form, setForm] = useState({
-    // name: '',
-    // surname: '',
-    // age: '',
-    // sex: '',
-    // hobbies: ['', '', ''],
-  })
+  const [form, setForm] = useState({})
   const [isError, setError] = useState(false)
 
   function handelInputChange(event) {
@@ -38,72 +32,48 @@ function App() {
     setPerson({ ...person, [key]: form[key] })
   }
 
-
-// function heandelNameInpuChange(event) {
-//   setForm({ ...form, name: event.target.value })
-// }
-// function handelSendNameBtnClick() {
-//   if (!form.name) {
-//     setError(true)
-//     return
-//   }
-
-//   setError(false)
-//   setPerson({ ...person, name: form.name })
-// }
-
-// function heandelSurNameInpuChange(event) {
-//   setForm({ ...form, surname: event.target.value })
-// }
-
-// function handelSendSurNameBtnClick() {
-//   if (!form.surname) {
-//     setError(true)
-//     return
-//   }
-//   setError(false)
-//   setPerson({ ...person, surname: form.surname })
-// }
-
-// function heandelAgeInpuChange(event) {
-//   setForm({ ...form, age: event.target.value })
-// }
-// function handelAgeBtnClick() {
-//   if (!form.age) {
-//     setError(true)
-//     return
-//   }
-//   setError(false)
-//   setPerson({ ...person, age: parseInt(form.age) })
-// }
-
 return (
   <div>
     {isError ? (<p>ошибка формы</p>) : ''}
     <input type="text"
       name="name"
-      // value={form.name}
       onChange={handelInputChange} />
     <button onClick={() => handelSetValueBtnClick('name')}>задать имя</button>
 
     <input type="text"
       name="surname"
-      // value={form.surname}
       onChange={handelInputChange} />
-    <button onClick={() => handelSetValueBtnClick('surname')}>задать имя</button>
+    <button onClick={() => handelSetValueBtnClick('surname')}>задать фамилию</button>
 
     <input type="number"
       name="age"
-      // value={form.age}
       onChange={handelInputChange} />
-    <button onClick={() => handelSetValueBtnClick('age')}>задать имя</button>
+    <button onClick={() => handelSetValueBtnClick('age')}>задать возраст</button>
+
+      <p>
+        пол:
+        <label>
+          <input type="radio" name='sex' value='male' checked={form.sex === 'male'} onChange={handelInputChange} /> мужской
+        </label>
+        <label>
+          <input type="radio" name='sex' value='female' checked={form.sex === 'female'} onChange={handelInputChange} /> женский
+        </label>
+        <button
+          onClick={() => handelSetValueBtnClick('sex')}
+        >задать пол</button>
+      </p>
+
+    <input type="text"
+      name="interest"
+      onChange={handelInputChange} />
+    <button onClick={() => handelSetValueBtnClick('interest')}>добавить интерес</button>
 
     <p>имя: {person.name}</p>
     <p>фамилия: {person.surname}</p>
     <p>возраст: {person.age}</p>
-    <p>пол: {person.sex}</p>
+      <p>sex: {person.sex === 'male' ? 'мужской' : 'женский'}</p>
     <p>interes: </p>
-    <div>{person.hobbies.map(function (item) {
+    <div>{person.interest.map(function (item) {
       return (
         <p key={item}>{item}</p>
       )
